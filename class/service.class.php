46766a7c697ec99d3af17801f5161d39d22b8864
Nimesh Ariyarathne <ariyarathnanimesh@gmail.com>
2021-12-22 04:27:31 +0530

    Service classes
    Service classes created

---
+++ class/service.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT']."/OOSD-with-MVC/OOSD-with-MVC/includes/autoloader.inc.php";
+
+class Pass extends Dbh{
+    private $service_no;
+    private $id;
+    private $name;
+    private $state
+
+}
