c9d40fa6e568c4bf73b3f5b908a169af6c76c7cd
Achira Dias <80286616+Invader-hub@users.noreply.github.com>
2021-12-22 18:23:04 +0530

    Tracker with singleton

--- class/essentialServiceTracker.class.php
+++ class/essentialServiceTracker.class.php
@@ -1,7 +1,15 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/OOSD-with-MVC/includes/autoloader.inc.php";
-// Tracker Class
+// Tracker Class Singelton
 class EssentialServiceTracker extends Tracker{
-
-
+    private EssentialServiceTracker $instance;
+    private function __construct(){
+    }
+    
+    public function getInstance(){
+        if ($this->instance == null) {
+            $this->instance = new EssentialServiceTracker();
+        }
+        return $this->instance;  
+    }
 }
