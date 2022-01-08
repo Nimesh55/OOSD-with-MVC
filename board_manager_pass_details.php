74fcb046b59c0e3fb4d1b765352d4d01b9bfa0a5
Sathira443 <liyanapathiranasathira@gmail.com>
2022-01-08 21:42:05 +0530

    Unneccessary ' removed

--- board_manager_pass_details.php
+++ board_manager_pass_details.php
@@ -69,11 +69,11 @@ $details = $board_manager_view->getApprovedPassesDetails();
 
     <div class="container mt-3" id="contanier-data">
         <!-- SearchBar -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    
         <form action="board_manager_pass_details.php" method="GET">
             <div class="row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-8">
+                <div class="col-sm-1 p-3"></div>
+                <div class="col-sm-10 p-3">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control" placeholder="Search with NIC Number" id="txtSearch" />
                         <div class="input-group-btn">
@@ -83,7 +83,7 @@ $details = $board_manager_view->getApprovedPassesDetails();
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-2"></div>
+                <div class="col-sm-1 p-3"></div>
             </div>
         </form>
         <br>
@@ -117,7 +117,7 @@ $details = $board_manager_view->getApprovedPassesDetails();
                     
                 }
 
-                ?>`
+                ?>
 
             </tbody>
         </table>
