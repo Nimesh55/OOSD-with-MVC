224026907016c263cf874a071ff237298453badd
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-01-08 17:19:25 +0530

    new

--- includes/executive_essential_service_details.inc.php
+++ includes/executive_essential_service_details.inc.php
@@ -1,36 +1,45 @@
 <?php 
 
 require_once $_SERVER['DOCUMENT_ROOT']."/OOSD-with-MVC/includes/autoloader.inc.php";
-if (isset($_POST['variablePass1'])) {
-    $str =  $_POST['variablePass1'];
-    print_r($str);
-    $seperatedButtonInput = explode("-", $str);
-    $button_state = $seperatedButtonInput[0];
-    $button_service_no = $seperatedButtonInput[1];
 
-    echo $button_service_no;
+echo "<pre>";
+print_r($_POST);
+echo "<pre>";
 
-    $ctrl_obj = new Executive_Controller();
-    if ($button_state == 0) {
-        print_r($_FILES);
-        //exit();
-        // echo "Yahoo";
-        // echo $button_service_no;
-        $ctrl_obj->setEssentialServiceState(0, $button_service_no);
-        $ctrl_obj->setExecutiveState(0, $button_service_no);
-    }
-    elseif($button_state == 1)
-    {
-        //echo "No= ".$button_service_no;
-        print_r($_FILES);
-        //exit();
-        $ctrl_obj->setEssentialServiceState(1, $button_service_no);
-        $ctrl_obj->setExecutiveState(1, $button_service_no);
-    }
+if($_POST['variablePass1']){
 
-    header("Location: ../executive_essential_service_details.php");
-    return;
 }
+
+//if (isset($_POST['variablePass1'])) {
+//    $str =  $_POST['variablePass1'];
+//    print_r($str);
+//    $seperatedButtonInput = explode("-", $str);
+//    $button_state = $seperatedButtonInput[0];
+//    $button_service_no = $seperatedButtonInput[1];
+//
+//    echo $button_service_no;
+//
+//    $ctrl_obj = new Executive_Controller();
+//    if ($button_state == 0) {
+//        print_r($_FILES);
+//        //exit();
+//        // echo "Yahoo";
+//        // echo $button_service_no;
+//        $ctrl_obj->setEssentialServiceState(0, $button_service_no);
+//        $ctrl_obj->setExecutiveState(0, $button_service_no);
+//    }
+//    elseif($button_state == 1)
+//    {
+//        //echo "No= ".$button_service_no;
+//        print_r($_FILES);
+//        //exit();
+//        $ctrl_obj->setEssentialServiceState(1, $button_service_no);
+//        $ctrl_obj->setExecutiveState(1, $button_service_no);
+//    }
+//
+//    header("Location: ../executive_essential_service_details.php");
+//    return;
+//}
 
 return;
 
