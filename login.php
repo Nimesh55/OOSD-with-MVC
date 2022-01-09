c0e2a08119e4bd37a580c78fde27c1a1d235d684
Sathira443 <liyanapathiranasathira@gmail.com>
2022-01-09 17:31:45 +0530

    Log in interface

--- login.php
+++ login.php
@@ -14,18 +14,29 @@
 </head>
 
 <body>
-    <div class="login-dark">
-    <div class="background-image"></div>
-    <h1>SAFE TRANSIT</h1>
-        <form action="includes/login.inc.php" method="post">
-            <div class="illustration"><img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-bus-automobile-kiranshastry-gradient-kiranshastry.png"/></div>
-            <div class="form-group"><input class="form-control" type="text" name="id" placeholder="ID" required=""></div>
-            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password" required=""></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 head-text">
+                <h1>SAFE TRANSIT</h1>
+            </div>
+            <div class="col-sm-6">
+            <div class="login-dark">
+                <div class="background-image"></div>
+                <form action="includes/login.inc.php" method="post">
+                    <div class="illustration"><img src="img/bus2.png" /></div>
+                    <div class="form-group"><input class="form-control insert" type="text" name="id" placeholder="ID" required=""></div>
+                    <div class="form-group"><input class="form-control insert" type="password" name="password" placeholder="Password" required=""></div>
 
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="submit">Log In</button></div><div class="sign_up">Don't have account? <a href="account_type.php" class="signup"> Sign up here</a></div></form>
+                    <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="submit">Log In</button></div>
+                    <div class="sign_up">Don't have account? &nbsp;<a href="account_type.php" class="signup">Sign up here</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
