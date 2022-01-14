5ac99eabb6754feb7fc0eb8f5f7abc8971033301
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-01-15 00:06:20 +0530

    upload button fixed

--- includes/passenger_register_in_company_2.inc.php
+++ includes/passenger_register_in_company_2.inc.php
@@ -2,11 +2,12 @@
     <div class="row">
         <div class="col-lg-3 cyan"></div>
         <div class="col-lg-6 wrapper">
-            <form class="form-horizontal" role="form" action="includes/passenger_register_in_company.inc.php" method="post" enctype="multipart/form-data">
+            <form class="form-horizontal" role="form" action="includes/passenger_register_in_company.inc.php"
+                  method="post" enctype="multipart/form-data">
                 <?php
                 if (isset($_POST['error']) && strcmp($_POST['error'], "identified") == 0) {
 
-                    echo "<div class=\"alert alert-danger\"><strong>".'*Enter the correct Staff Id!!!'."</strong></div>";
+                    echo "<div class=\"alert alert-danger\"><strong>" . '*Enter the correct Staff Id!!!' . "</strong></div>";
                 }
                 ?>
                 <div class="form-group">
@@ -15,7 +16,7 @@
                         <select name="service_no" id="service" class="form-control">
 
                             <?php
-                            foreach ($services as $service){
+                            foreach ($services as $service) {
 
                                 echo "<option value=\"{$service['service_no']}\">{$service['name']}</option>";
                             }
@@ -38,8 +39,26 @@
                 <div class="form-group">
                     <label for="file" class="col-sm-3 control-label">Select a file:</label>
                     <div class="col-sm-9">
-                        <input type="file" id="file" class="form-control" name="file"/>
+<!--                        <input type="file" id="file" class="form-control" name="file"/>-->
+
+                        <div class="input-group">
+                            <input type="text" class="form-control" readonly>
+                            <label class="input-group-btn">
+                                <span class="btn btn-primary">
+                                    Browse File <input type="file" id="file" name="file" style="display: none;">
+                                </span>
+                            </label>
+                        </div>
+
                     </div>
+
+                    <div class="col-lg-6 col-sm-6 col-12">
+
+
+
+                    </div>
+
+
                 </div>
 
                 <br>
