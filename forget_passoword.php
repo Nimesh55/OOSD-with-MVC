802c1bec1ac16f1ee001f69486ffee5c355d546c
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-01-15 22:52:32 +0530

    forget password interface finished

---
+++ forget_passoword.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forget Password</title>
+
+    <link rel="stylesheet" href="css/passenger_profile_edit.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+
+
+
+</head>
+<body>
+
+<div class="container heading">
+    <h1>Forget Password?</h1>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 cyan"></div>
+        <div class="col-lg-6 pink wrapper">
+
+<!--            use this php block if you want to display some alert-->
+<!--            --><?php
+//            if (isset($_POST['error_str']) && strcmp($_POST['error_str'],"Success")!=0) {
+//
+//                echo "<div class=\"alert alert-danger\"><strong>".$error_str."</strong></div>";
+//            }
+//            if(isset($_POST['error_str']) && strcmp($_POST['error_str'],"Success")==0){
+//                echo "<div class=\"alert alert-success\"><strong>"."Successfully Updated!!!"."</strong></div>";
+//            }
+//            ?>
+            <form class="form-horizontal" role="form" action="includes/passenger_edit_profile.inc.php" method="post">
+                <div class="form-group">
+                    <label for="fname" class="col-sm-3 control-label">User ID:</label>
+                    <div class="col-sm-9">
+                        <input name="fname" type="text" class="form-control" id="user_id">
+                    </div>
+                </div>
+                <br>
+                <div class="btn-group btn-group-lg">
+                    <input type="submit" class="btn btn-primary ctrlbutton" name="submit" value="Submit">
+                    <input type="submit" class="btn btn-primary ctrlbutton" name="exit" value="Exit">
+                </div>
+
+            </form>
+
+        </div>
+        <div class="col-lg-3 orange"></div>
+    </div>
+</div>
+
+</body>
+</html>
