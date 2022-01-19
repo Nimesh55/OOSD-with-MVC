cdf9bdd7f8d9ae057fbc16d31098a403ca352194
Sathira443 <liyanapathiranasathira@gmail.com>
2022-01-20 00:29:49 +0530

    navigation bar in edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -1,37 +1,37 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT']."/OOSD-with-MVC/includes/autoloader.inc.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/OOSD-with-MVC/includes/autoloader.inc.php";
 session_start();
 
-if(!isset($_SESSION['account_no'])){
+if (!isset($_SESSION['account_no'])) {
     header("Location: login.php");
     return;
 }
 
-$error_str='';
-if($_SESSION['account_type']==0){
+$error_str = '';
+if ($_SESSION['account_type'] == 0) {
     $passenger_view = new Passenger_View($_SESSION['user_Id']);
     $row = $passenger_view->getDetails();
-}elseif ($_SESSION['account_type']==1){
+} elseif ($_SESSION['account_type'] == 1) {
     $conductor_view = new Conductor_View($_SESSION['user_Id']);
     $row = $conductor_view->getDetails();
-}else{
+} else {
     $executive_view = new Executive_View();
     $row = $executive_view->getDetails();
 }
 
-if (isset($_SESSION["error"]) && strcmp($_SESSION["error"],"Success")!=0) {
-    $row['first_name']=$_POST['fname'];
-    $row['last_name']=$_POST['lname'];
-    $row['address']=$_POST['address'];
-    $row['telephone']=$_POST['telephone'];
-    $row['email']=$_POST['email'];
-
+if (isset($_SESSION["error"]) && strcmp($_SESSION["error"], "Success") != 0) {
+    $row['first_name'] = $_POST['fname'];
+    $row['last_name'] = $_POST['lname'];
+    $row['address'] = $_POST['address'];
+    $row['telephone'] = $_POST['telephone'];
+    $row['email'] = $_POST['email'];
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -56,71 +56,98 @@ if (isset($_SESSION["error"]) && strcmp($_SESSION["error"],"Success")!=0) {
 
 
 </head>
+
 <body>
 
-<div class="container heading">
-    <h1>Edit Profile</h1>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-3 cyan"></div>
-        <div class="col-lg-6 pink wrapper">
+    <div class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
 
-            <?php
-            if (isset($_SESSION["error"]) && strcmp($_SESSION["error"],"Success")!=0) {
+                    <div class="navbar-header">
+                        <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                        <strong class="navbar-brand">Safe Transit</strong>
+                    </div>
 
-                echo "<div class=\"alert alert-danger\"><strong>".$_SESSION["error"]."</strong></div>";
-            }
-            if(isset($_SESSION["error"]) && strcmp($_SESSION["error"],"Success")==0){
-                echo "<div class=\"alert alert-success\"><strong>"."Successfully Updated!!!"."</strong></div>";
-            }
-            ?>
+                    <div class="navbar-collapse collapse" id="mobile_menu">
 
-            <form class="form-horizontal" role="form" action="includes/edit_profile.inc.php" method="post">
-                <div class="form-group">
-                    <label for="fname" class="col-sm-3 control-label">First Name:</label>
-                    <div class="col-sm-9">
-                        <input name="fname" type="text" class="form-control" id="fname" value="<?= $row['first_name'];?>">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo $row['first_name']." ".$row['last_name'] ?> <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="includes/logout.inc.php">Log out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="lname" class="col-sm-3 control-label">Last Name:</label>
-                    <div class="col-sm-9">
-                        <input name="lname" type="text" class="form-control" id="lname" value="<?= $row['last_name']; ?>">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="address" class="col-sm-3 control-label">Address:</label>
-                    <div class="col-sm-9">
-                        <textarea class="form-control" name="address" rows="5" id="address"><?= $row['address']; ?></textarea>
-                    </div>
-                </div>
-
-
-                <div class="form-group">
-                    <label for="email" class="col-sm-3 control-label">Email:</label>
-                    <div  class="col-sm-9"><input name="email" type="text" class="form-control" id="email" value="<?= $row['email']; ?>"></div>
-                </div>
-                <div class="form-group">
-                    <label for="telephone" class="col-sm-3 control-label">Telephone:</label>
-                    <div class="col-sm-9"><input name="telephone" type="telephone" class="form-control" id="telephone" value="<?= $row['telephone']; ?>"></div>
-                </div>
-                <br>
-                <div class="btn-group btn-group-lg">
-                    <input type="submit" class="btn btn-primary ctrlbutton" name="cpwd" value="Change Password">
-                    <input type="submit" class="btn btn-primary ctrlbutton" name="save" value="Save">
-                    <input type="submit" class="btn btn-primary ctrlbutton" name="back" value="Back">
-                </div>
-
-            </form>
-
+            </div>
         </div>
-        <div class="col-lg-3 orange"></div>
     </div>
-</div>
+
+    <div class="container heading">
+        <h1>Edit Profile</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 cyan"></div>
+            <div class="col-lg-6 pink wrapper">
+
+                <?php
+                if (isset($_SESSION["error"]) && strcmp($_SESSION["error"], "Success") != 0) {
+
+                    echo "<div class=\"alert alert-danger\"><strong>" . $_SESSION["error"] . "</strong></div>";
+                }
+                if (isset($_SESSION["error"]) && strcmp($_SESSION["error"], "Success") == 0) {
+                    echo "<div class=\"alert alert-success\"><strong>" . "Successfully Updated!!!" . "</strong></div>";
+                }
+                ?>
+
+                <form class="form-horizontal" role="form" action="includes/edit_profile.inc.php" method="post">
+                    <div class="form-group">
+                        <label for="fname" class="col-sm-3 control-label">First Name:</label>
+                        <div class="col-sm-9">
+                            <input name="fname" type="text" class="form-control" id="fname" value="<?= $row['first_name']; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="lname" class="col-sm-3 control-label">Last Name:</label>
+                        <div class="col-sm-9">
+                            <input name="lname" type="text" class="form-control" id="lname" value="<?= $row['last_name']; ?>">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="address" class="col-sm-3 control-label">Address:</label>
+                        <div class="col-sm-9">
+                            <textarea class="form-control" name="address" rows="5" id="address"><?= $row['address']; ?></textarea>
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="email" class="col-sm-3 control-label">Email:</label>
+                        <div class="col-sm-9"><input name="email" type="text" class="form-control" id="email" value="<?= $row['email']; ?>"></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="telephone" class="col-sm-3 control-label">Telephone:</label>
+                        <div class="col-sm-9"><input name="telephone" type="telephone" class="form-control" id="telephone" value="<?= $row['telephone']; ?>"></div>
+                    </div>
+                    <br>
+                    <div class="btn-group btn-group-lg">
+                        <input type="submit" class="btn btn-primary ctrlbutton" name="cpwd" value="Change Password">
+                        <input type="submit" class="btn btn-primary ctrlbutton" name="save" value="Save">
+                        <input type="submit" class="btn btn-primary ctrlbutton" name="back" value="Back">
+                    </div>
+
+                </form>
+
+            </div>
+            <div class="col-lg-3 orange"></div>
+        </div>
+    </div>
 
 </body>
+
 </html>
 
 <?php
