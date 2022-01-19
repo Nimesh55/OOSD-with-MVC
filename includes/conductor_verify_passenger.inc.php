c8a89642f9e5a2441425737fc8ec1dcb32163623
Sathira443 <liyanapathiranasathira@gmail.com>
2022-01-19 23:50:19 +0530

    Error corretced

--- includes/conductor_verify_passenger.inc.php
+++ includes/conductor_verify_passenger.inc.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
-
+require_once "../class/Model/dbh.class.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/OOSD-with-MVC/includes/autoloader.inc.php";
 
 if (isset($_POST['submit'])) {
 
     // fetch passenger id
     $passenger_id = htmlentities($_POST['passenger_id']);
 
-    // Instanciate Passenger Controller in MVC
-    require_once "../class/dbh.class.php";
-    require_once "../class/conductor.class.php";
-    require_once "../class/conductor_controller.class.php";
-    require_once "../class/conductor_model.class.php";
+    // // Instanciate Passenger Controller in MVC
+    
+    // require_once "../class/conductor.class.php";
+    // require_once "../class/Controller/conductor_controller.class.php";
+    // require_once "../class/Model/conductor_model.class.php";
     
     
     $conductor_ctrl_obj = new Conductor_Controller();
