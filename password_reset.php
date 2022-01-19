984a937f629ee6232d7699f4fe5e7b5ef5d75124
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-01-19 22:10:22 +0530

    password toggle visibility

--- password_reset.php
+++ password_reset.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/OOSD-with-MVC/includes/autoloader.inc.php";
 session_start();
-if(!isset($_SESSION['user_Id'])){
+if (!isset($_SESSION['user_Id'])) {
     header("Location: login.php");
     return;
 }
@@ -27,12 +27,12 @@ if(!isset($_SESSION['user_Id'])){
         <div class="row">
             <div class="col-sm-1"></div>
             <div class="col-sm-10 wrapper">
-                <form method="POST" class="form-horizontal" action="includes/change_password.inc.php ?>" >
+                <form method="POST" class="form-horizontal" action="includes/change_password.inc.php ?>">
 
                     <?php
-                    if(isset($_SESSION["error"])){
+                    if (isset($_SESSION["error"])) {
                         $error = $_SESSION["error"];
-                        echo "<div class=\"alert alert-danger\"><strong>".$error."</strong></div>";
+                        echo "<div class=\"alert alert-danger\"><strong>" . $error . "</strong></div>";
 
                     }
                     ?>
@@ -40,27 +40,41 @@ if(!isset($_SESSION['user_Id'])){
                     <!--                <div class="form-group">-->
                     <!--                    <label class="control-label col-sm-3" for="current">Current Password:</label>-->
                     <!--                    <div class="col-sm-10">-->
-                    <input type="password" name="current_password" value="-1"  hidden>
+                    <input type="password" name="current_password" value="-1" hidden>
                     <!--                    </div>-->
                     <!--                </div>-->
                     <div class=" form-group">
                         <label class="control-label col-sm-3" for="new">New Password:</label>
                         <div class="col-sm-9">
-                            <input type="password" name="new_password" class="form-control" id="new" placeholder="Enter New Password">
+                            <div class="input-group">
+                                <input type="password" name="new_password" class="form-control new_pwd" id="new"
+                                       placeholder="Enter New Password">
+                                <span class="input-group-btn">
+                                        <button class="btn btn-default reveal_2" type="button"><i
+                                                    class="glyphicon glyphicon-eye-open"></i></button>
+                                    </span>
+                            </div>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label class="control-label col-sm-3" for="retype_new">ReEnter New Password:</label>
                         <div class="col-sm-9">
-                            <input type="password" name="retype_password"  class="form-control" id="retype_new" placeholder="ReEnter New Password">
+                            <div class="input-group">
+                                <input type="password" name="retype_password" class="form-control retype_pwd" id="retype_new"
+                                       placeholder="ReEnter New Password">
+                                <span class="input-group-btn">
+                                        <button class="btn btn-default reveal_3" type="button"><i
+                                                    class="glyphicon glyphicon-eye-open"></i></button>
+                                    </span>
+                            </div>
                         </div>
                     </div>
 
-<div class="btn-group btn-group-lg">
-                            <input name='reset' value="Reset Password" type="submit" class="btn btn-primary ctrlbutton">
-                            <input name='cancel' value="Cancel" type="submit" class="btn btn-primary ctrlbutton" >
-</div>
+                    <div class="btn-group btn-group-lg">
+                        <input name='reset' value="Reset Password" type="submit" class="btn btn-primary ctrlbutton">
+                        <input name='cancel' value="Cancel" type="submit" class="btn btn-primary ctrlbutton">
+                    </div>
 
                 </form>
             </div>
@@ -68,6 +82,7 @@ if(!isset($_SESSION['user_Id'])){
         </div>
 
     </div>
+    <script src="js/change_pwd.js"></script>
     </body>
 
     </html>
