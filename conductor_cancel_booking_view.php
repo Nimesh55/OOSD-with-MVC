458871d50fe45c657cd1951c09f3153901a6f8fa
Sathira443 <liyanapathiranasathira@gmail.com>
2022-01-21 01:30:57 +0530

    Conductor View heading added

--- conductor_cancel_booking_view.php
+++ conductor_cancel_booking_view.php
@@ -30,11 +30,13 @@ if ($_GET["error"] != 'none') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
     <link rel="stylesheet" href="css/conductor_cancel_booking_view.css">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <title>Conductor || Booking Details</title>
 </head>
 
@@ -68,8 +70,14 @@ if ($_GET["error"] != 'none') {
         </div>
     </div>
 
-    <div class="container mt-3" id="contanier-data">
+    <div class="container">
+        <div class="heading">
+            <h1>Booking No. <?php echo $bookingNo ?> Details</h1>
+        </div>
+    </div>
 
+
+    <div class="container mt-3" id="contanier-data">
         <div>
             <div class="row" id="error">
                 <div class="col-sm-2 p-2"></div>
@@ -194,9 +202,9 @@ if ($_GET["error"] != 'none') {
             <br>
 
             <?php if ($bookingDetails['flag'] == 0) : ?>
-            <div class="ctrlbtn">
-                <a class="btn btn-primary" href="includes/condcutor_cancel_booking_totally.inc.php?booking_no=<?php echo $bookingNo ?>">Cancel Booking</a>
-            </div>
+                <div class="ctrlbtn">
+                    <a class="btn btn-primary" href="includes/condcutor_cancel_booking_totally.inc.php?booking_no=<?php echo $bookingNo ?>">Cancel Booking</a>
+                </div>
 
             <?php endif; ?>
 
