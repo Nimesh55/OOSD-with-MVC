b7aad211657daff964f5833b096bddaa1a82583d
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-02-09 23:25:22 +0530

    center

--- administrator_home.php
+++ administrator_home.php
@@ -35,7 +35,7 @@ $completedBookings = $viewArray["completedBooking"];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/passenger_home.css">
+    <link rel="stylesheet" href="css/administrator_home.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
@@ -80,7 +80,7 @@ $completedBookings = $viewArray["completedBooking"];
         </div>
     </div>
 
-    <div class="container mt-3">
+    <div class="container">
         <!--        <h1> --><? //= $_SESSION['user_Id'] 
                             ?>
         <!-- </h1>-->
@@ -121,13 +121,16 @@ $completedBookings = $viewArray["completedBooking"];
                         <p><?php echo $numofIssuedPasses ?></p>
                     </div>
                 </div>
-
+                <br>
+                <br>
                 <div class="row" style="background-color:white;border-radius:10px;text-align:center;">
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-10"><canvas id="myChart1" style="width:100%;max-width:600px;color:aliceblue;"></canvas>
+<!--                    <div class="col-sm-1"></div>-->
+                    <div class="col-sm-6">
+                        <canvas id="myChart1" style="width:100%;max-width:600px;color:aliceblue;"></canvas>
+                    </div>
+                    <div class="col-sm-6">
                         <canvas id="myChart2" style="width:100%;max-width:600px;color:aliceblue;"></canvas>
                     </div>
-                    <div class="col-sm-1"></div>
                 </div>
             </div>
             <div class="col-lg-1"></div>
