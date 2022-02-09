7dbf838f2beb5f575de4a37794912177b6d229b5
Achira Dias <80286616+Invader-hub@users.noreply.github.com>
2022-02-09 21:03:41 +0530

    graph in admin

--- administrator_home.php
+++ administrator_home.php
@@ -12,8 +12,19 @@ $view = new Administrator_View();
 $viewArray = $view->getDetails();
 $numofpendingCompanies = $viewArray["pending"];
 $numofServicesApproved = $viewArray["approved"];
-$numofIssuedPasses = $viewArray["issued"];
 
+$numofIssuedPasses = $viewArray["issued"];
+$pendingPasses = $viewArray["pendingPass"];
+$declinedPass = $viewArray["declinedPass"];
+$acceptedPass = $viewArray["acceptedPass"];
+$confirmedPass = $viewArray["confirmed"];
+$expiredPass = $viewArray["expiredPass"];
+
+// $numofIssuedBookings = $viewArray["issued"];
+// $pendingBookings = $viewArray["pendingPass"];
+// $declinedBookings = $viewArray["declinedPass"];
+// $acceptedBookings = $viewArray["acceptedPass"];
+// $expiredBookings = $viewArray[""];
 ?>
 
 <!DOCTYPE html>
@@ -27,6 +38,7 @@ $numofIssuedPasses = $viewArray["issued"];
     <link rel="stylesheet" href="css/passenger_home.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -35,87 +47,187 @@ $numofIssuedPasses = $viewArray["issued"];
 </head>
 
 <body>
-<div class="navbar navbar-inverse">
-    <div class="container-fluid">
+    <div class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="navbar-header">
+                        <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                        </button>
+                        <strong class="navbar-brand">Safe Transit</strong>
+                    </div>
+
+                    <div class="navbar-collapse collapse" id="mobile_menu">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="administrator_home.php">Home</a></li>
+                            <li><a href="administrator_pending_essential_services.php">Pending Essential Services</a></li>
+                            <li><a href="administrator_approved_essential_services.php">Approved Essential Services</a></li>
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo 'Administrator'; ?> <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="administrator_configuration_settings.php">Settings</a></li>
+                                    <li><a href="change_password.php">Change Password</a></li>
+                                    <li><a href="includes/logout.inc.php">Log out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-3">
+        <!--        <h1> --><? //= $_SESSION['user_Id'] 
+                            ?>
+        <!-- </h1>-->
+        <div class="heading">
+            <h1 id="heading"> <?= 'Administrator'; ?> </h1>
+        </div>
+
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-10 wrapper">
 
-                <div class="navbar-header">
-                    <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse"><span
-                                class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                    </button>
-                    <strong class="navbar-brand">Safe Transit</strong>
+                <div class="row">
+                    <div class="col-sm-6 p-3 field">
+                        <p>Number of Pending Companies</p>
+                    </div>
+                    <div class="col-sm-1 p-3">:</div>
+                    <div class="col-sm-5 p-3">
+                        <p><?php echo $numofpendingCompanies ?></p>
+                    </div>
                 </div>
 
-                <div class="navbar-collapse collapse" id="mobile_menu">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="administrator_home.php">Home</a></li>
-                        <li><a href="administrator_pending_essential_services.php">Pending Essential Services</a></li>
-                        <li><a href="administrator_approved_essential_services.php">Approved Essential Services</a></li>
-                    </ul>
+                <div class="row">
+                    <div class="col-sm-6 p-3 field">
+                        <p>Number of Aprrroved Companies</p>
+                    </div>
+                    <div class="col-sm-1 p-3">:</div>
+                    <div class="col-sm-5 p-3">
+                        <p><?php echo $numofServicesApproved ?></p>
+                    </div>
+                </div>
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span
-                                        class="glyphicon glyphicon-user"></span> <?php echo 'Administrator'; ?> <span
-                                        class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="administrator_configuration_settings.php">Settings</a></li>
-                                <li><a href="change_password.php">Change Password</a></li>
-                                <li><a href="includes/logout.inc.php">Log out</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                <div class="row">
+                    <div class="col-sm-6 p-3 field">
+                        <p>Number of Issued Passes</p>
+                    </div>
+                    <div class="col-sm-1 p-3">:</div>
+                    <div class="col-sm-5 p-3">
+                        <p><?php echo $numofIssuedPasses ?></p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6  field">
+                        <canvas id="myChart1" style="width:100%;max-width:600px;color:aliceblue;"></canvas>
+                    </div>
+                    <div class="col-sm-6 "><canvas id="myChart2" style="width:100%;max-width:600px;color:aliceblue;"></canvas></div>
+
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 ">
+                        <canvas id="myChart3" style="width:100%;max-width:600px;color:aliceblue;"></canvas>
+                    </div>
                 </div>
             </div>
+            <div class="col-lg-1"></div>
+
         </div>
-    </div>
-</div>
-
-<div class="container mt-3">
-    <!--        <h1> --><? //= $_SESSION['user_Id'] ?><!-- </h1>-->
-    <div class="heading">
-        <h1 id="heading"> <?= 'Administrator'; ?> </h1>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-3"></div>
-        <div class="col-lg-6 wrapper">
-
-            <div class="row">
-                <div class="col-sm-6 p-3 field">
-                    <p>Number of Pending Companies</p>
-                </div>
-                <div class="col-sm-1 p-3">:</div>
-                <div class="col-sm-5 p-3">
-                    <p><?php echo $numofpendingCompanies ?></p>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-6 p-3 field">
-                    <p>Number of Aprrroved Companies</p>
-                </div>
-                <div class="col-sm-1 p-3">:</div>
-                <div class="col-sm-5 p-3">
-                    <p><?php echo $numofServicesApproved ?></p>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-6 p-3 field">
-                    <p>Number of Issued Passes</p>
-                </div>
-                <div class="col-sm-1 p-3">:</div>
-                <div class="col-sm-5 p-3">
-                    <p><?php echo $numofIssuedPasses ?></p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3"></div>
 
     </div>
 
-</div>
+    <script>
+        var xValues = ["Pending", "Accepted","Confirmed", "Declined", "Expired"];
+        var yValues = [<?php echo $pendingPasses ?>, <?php echo $acceptedPass ?>, <?php echo $confirmedPass ?>, <?php echo $declinedPass ?>, <?php echo $expiredPass ?>];
+        var barColors = [
+            "#e1e990",
+            "#00aba9",
+            "#2b5797",
+            "#e8c3b9",
+            "#1e7145"
+        ];
+
+        new Chart("myChart1", {
+            type: "doughnut",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Passes"
+                }
+            }
+        });
+    </script>
+
+    <script>
+        var xValues = ["Pending", "Approved", "Declined"];
+        var yValues = [<?php echo $numofpendingCompanies ?>, <?php echo $numofServicesApproved ?>, <?php echo $numofIssuedPasses ?>];
+        var barColors = [
+            "#b91d47",
+            "#00aba9",
+            "#2b5797",
+            "#e8c3b9",
+            "#1e7145"
+        ];
+
+        new Chart("myChart2", {
+            type: "doughnut",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Bookings"
+                }
+            }
+        });
+    </script>
+
+    <script>
+        var xValues = ["Pending", "Approved", "Declined"];
+        var yValues = [<?php echo 0 ?>, <?php echo $numofServicesApproved ?>, <?php echo $numofIssuedPasses ?>];
+        var barColors = [
+            "#b91d47",
+            "#00aba9",
+            "#2b5797",
+            "#e8c3b9",
+            "#1e7145"
+        ];
+
+        new Chart("myChart3", {
+            type: "doughnut",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Passengers"
+                }
+            }
+        });
+    </script>
+
 </body>
 
 </html>
--- class/View/administrator_view.class.php
+++ class/View/administrator_view.class.php
@@ -1,44 +1,80 @@
 
 <?php
-  require_once $_SERVER['DOCUMENT_ROOT']."/OOSD-with-MVC/includes/autoloader.inc.php";
-  class Administrator_View extends Administrator_Model{
-    private $adminobj;
-    private $adminctrl;
+require_once $_SERVER['DOCUMENT_ROOT'] . "/OOSD-with-MVC/includes/autoloader.inc.php";
+class Administrator_View extends Administrator_Model
+{
+  private $adminobj;
+  private $adminctrl;
 
-    public function __construct(){
-        $this->adminctrl = new Administrator_Controller;
-        $this->adminobj = $this->adminctrl->setUpDetails();
-    }
-    public function getDetails()
-    {
-      $details=array(
-                "uid"=> $this->adminobj->getUid(),
-                "pending"=> $this->adminobj->getnumPendingCompany(),
-                "approved"=> $this->adminobj->getnumApprovedService(),
-                "issued"=> $this->adminobj->getnumIssuedPasses());
-
-      return $details;
-    }
-
-    public function getPendingRows(){
-      return $this->getPendingEssentialServices();
-    }
-
-    public function getApprovedRows(){
-      return $this->getApprovedEssentialServices();
-    }
-
-    public function fetchDetails($service_no){
-      $row = $this->adminctrl->formatForView($service_no);
-      $details=array(
-        "service_no"=> $row['service_no'],
-        "id"=> $row['id'],
-        "name"=> $row['name'],
-        "state"=> $row['state'],
-        "file_no"=> $row['file_no']);
-      return $details;
-    }
-    public function getEmailSettingsDetails(){
-        return $this->getNotificationConfigData();
-    }
+  public function __construct()
+  {
+    $this->adminctrl = new Administrator_Controller;
+    $this->adminobj = $this->adminctrl->setUpDetails();
   }
+  public function getDetails()
+  {
+    $passes = Pass_Tracker::getInstance()->getAllPasses();
+    $pendingPasses = array();
+    $declinedPass = array();
+    $acceptedPass = array();
+    $confirmedPass = array();
+    $expiredPass = array();
+    foreach ($passes as $pass) {
+      if ($pass->getState() == 0) {
+        array_push($pendingPasses, $pass);
+      } elseif ($pass->getState() == 1) {
+        array_push($acceptedPass, $pass);
+      } elseif ($pass->getState() == 2) {
+        array_push($confirmedPass, $pass);
+      } elseif ($pass->getState() == 3) {
+        array_push($expiredPass, $pass);
+      } elseif ($pass->getState() == 4) {
+        array_push($declinedPass, $pass);
+      }
+    }
+
+    $details = array(
+      "uid" => $this->adminobj->getUid(),
+      "pending" => $this->adminobj->getnumPendingCompany(),
+      "approved" => $this->adminobj->getnumApprovedService(),
+      "issued" => $this->adminobj->getnumIssuedPasses(),
+      "pendingPass" => count($pendingPasses),
+      "declinedPass" => count($declinedPass),
+      "acceptedPass" => count($acceptedPass),
+      "confirmed" => count($confirmedPass),
+      "expiredPass" => count($expiredPass),
+      "pendingBooking" => 100,
+      "approvedBooking" => 100,
+      "declinedBooking" => 100
+    );
+
+    return $details;
+  }
+
+  public function getPendingRows()
+  {
+    return $this->getPendingEssentialServices();
+  }
+
+  public function getApprovedRows()
+  {
+    return $this->getApprovedEssentialServices();
+  }
+
+  public function fetchDetails($service_no)
+  {
+    $row = $this->adminctrl->formatForView($service_no);
+    $details = array(
+      "service_no" => $row['service_no'],
+      "id" => $row['id'],
+      "name" => $row['name'],
+      "state" => $row['state'],
+      "file_no" => $row['file_no']
+    );
+    return $details;
+  }
+  public function getEmailSettingsDetails()
+  {
+    return $this->getNotificationConfigData();
+  }
+}
